d8811efc68b796d2c2326ecdabea2119eff6850e
sanjardev23 <mksanjar04@gmail.com>
2026-05-22 02:11:24 +0900

    BRR: Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+console.log("EXECUTED")
+
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD")
+console.log(currentTime)
+
+
+const person: string = 'Simon';
+const count: number = 100;
