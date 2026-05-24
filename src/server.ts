598a16c06ad9e3c242037bb587b67a41474ebce2
sanjardev23 <mksanjar04@gmail.com>
2026-05-25 02:42:19 +0900

    feat: integration of enviromental variables

--- src/server.ts
+++ src/server.ts
@@ -3,3 +3,27 @@
 // MVC = MODEL VIEW CONTROLLER
 
 // Design pattern: Middleware, Decorator
+
+
+// Burak - MVC Pattern
+// Nestar - MVC Pattern, Dependency Injection, Decorator Pattern ...
+
+
+ 
+// OOP - Object Orienter Programming
+// AOP - Aspect Oriented Programming
+// FRP - Functional Reactive Programming
+
+
+
+import moment from 'moment';            // this is in module js
+// const moment = require('moment');    // this is in common js
+
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
