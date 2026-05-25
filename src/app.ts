86a6b9a54bf6730eec9b8377b1ad2bb5f85b2f5d
sanjardev23 <mksanjar04@gmail.com>
2026-05-25 12:42:17 +0900

    feat: express integrations

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from "path";
+
+/** 1-ENTARANCE **/
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+/** 2-SESSIONS **/
+
+/** 3-VIEWS **/
+app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs")
+
+/** 4-ROUTES **/
+
+
+export default app;         // module.exports = app (similar to this)
--- src/server.ts
+++ src/server.ts
@@ -21,17 +21,28 @@
 import moment from 'moment';            // this is in module js
 // const moment = require('moment');    // this is in common js
 
+
+
+
+
+
 import dotenv from 'dotenv';
 dotenv.config();
-
-
 import mongoose from 'mongoose';
+import app from "./app"
+
+
+// MySQL > Table
+// Mongoose > Suniy Table > Collection
 
 mongoose
     .connect(process.env.MONGO_URL as string)
     .then((data) => {
         console.log('MongoDB connection succeed')
         const PORT = process.env.PORT ?? 3003;
+        app.listen(PORT, function () {
+            console.log(`The server is running successfully on port: ${PORT}`)
+        })
     })
     .catch((err) => console.log("ERROR on connection MongoDB", err));
 
