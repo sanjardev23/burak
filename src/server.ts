a60ca460b13f595848a9eb197b7e2213b01a55e6
sanjardev23 <mksanjar04@gmail.com>
2026-05-25 12:24:23 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -15,15 +15,25 @@
 // FRP - Functional Reactive Programming
 
 
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
 
 import moment from 'moment';            // this is in module js
 // const moment = require('moment');    // this is in common js
-
-
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 
+import mongoose from 'mongoose';
 
-// CLUSTER => DATABASE => COLLECTION => DOCUMENT
+mongoose
+    .connect(process.env.MONGO_URL as string)
+    .then((data) => {
+        console.log('MongoDB connection succeed')
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch((err) => console.log("ERROR on connection MongoDB", err));
+
+
+
