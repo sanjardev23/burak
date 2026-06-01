12a3d0aa941317449a50b5453a6bbf95d1f770e7
sanjardev23 <mksanjar04@gmail.com>
2026-06-02 03:05:16 +0900

    fix: modify restaurant login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import { MemberType } from "./../libs/enums/member.enum";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import MemberModel from "../schema/Member.model";
+import * as bcrypt from "bcryptjs";
 
 // Service layer — contains all the business logic
 // Controllers call this, this talks to the database
@@ -20,8 +21,10 @@ class MemberService {
     const exist = await this.memberModel
       .findOne({ memberType: MemberType.RESTAURANT })
       .exec();
-
     if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED); // block if already exists
+
+    const salt = await bcrypt.genSalt();
+    input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
 
     try {
       const result = await this.memberModel.create(input); // save new member to MongoDB
@@ -31,6 +34,8 @@ class MemberService {
       throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
     }
   }
+
+
 
 
   public async processLogin(input: LoginInput): Promise<Member> {
@@ -43,7 +48,11 @@ class MemberService {
 
     if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
 
-    const isMatch = input.memberPassword === member.memberPassword;
+    // const isMatch = input.memberPassword === member.memberPassword;
+    const isMatch = await bcrypt.compare(
+      input.memberPassword, 
+      member.memberPassword!
+     );
 
     if (!isMatch) {
         throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
