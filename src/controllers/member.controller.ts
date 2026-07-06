577e331d0686e147488357bb1bef505b14d98c68
sanjardev23 <mksanjar04@gmail.com>
2026-07-06 16:26:26 +0900

    feat: create middleware methods and develop logout process

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, Member, MemberInput } from "../libs/types/member";
+import {
+  ExtendenRequest,
+  LoginInput,
+  Member,
+  MemberInput,
+} from "../libs/types/member";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -50,19 +55,50 @@ memberController.login = async (req: Request, res: Response) => {
   }
 };
 
-memberController.verifyAuth = async (req: Request, res: Response) => {
+memberController.logout = async (req: ExtendenRequest, res: Response) => {
   try {
-    let member = null;
+    console.log("logout");
+    res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
+    res.status(HttpCode.OK).json({ logout: true });
+  } catch (err) {
+    console.log("Error on login", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
+
+memberController.verifyAuth = async (
+  req: ExtendenRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
     const token = req.cookies["accessToken"];
-    if (token) member = await authService.checkAuth(token);
-    if (!member)
+    if (token) req.member = await authService.checkAuth(token);
+    if (!req.member)
       throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
 
-    res.status(HttpCode.OK).json({ member: member });
+    next();
   } catch (err) {
     console.log("Error on verifyAuth", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
+
+memberController.retrieveAuth = async (
+  req: ExtendenRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const token = req.cookies["accessToken"];
+    if (token) req.member = await authService.checkAuth(token);
+
+    next();
+  } catch (err) {
+    console.log("Error on retrieveAuth", err);
+    next();
   }
 };
 
--- src/router.ts
+++ src/router.ts
@@ -14,12 +14,15 @@ import memberController from "./controllers/member.controller";
 
 router.post("/member/login", memberController.login);
 router.post("/member/signup", memberController.signup);
+router.post(
+  "/member/logout",
+  memberController.verifyAuth,
+  memberController.logout,
+);
 router.get("/member/detail", memberController.verifyAuth);
 
 //** Product **/
 
 //** Order **/
-
-
 
 export default router;
