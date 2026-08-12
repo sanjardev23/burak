8be2b4b3b6ca5919d4193e4fe0828eeee35e885b
sanjardev23 <mksanjar04@gmail.com>
2026-08-12 14:17:10 +0900

    fix: change server port configuration

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ mongoose
   .connect(process.env.MONGO_URL as string)
   .then((data) => {
     console.log("MongoDB connection succeed");
-    const PORT = process.env.PORT ?? 3003;
+    const PORT = process.env.PORT ?? 8000;
     app.listen(PORT, function () {
       console.log(`The server is running successfully on port: ${PORT}`);
       console.log(`Admin project on http://localhost:${PORT}/admin \n`);
